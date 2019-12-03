be24195b9fe293c06e5063619f25ebebcb95cf3d
yun8686 <yun.mes41@gmail.com>
2019-12-03 14:40:34 +0900

    形態素解析を追加

--- websites/src/page/menu/createWords.tsx
+++ websites/src/page/menu/createWords.tsx
@@ -1,46 +1,72 @@
 import * as React from 'react'
 import '../../scss/app.scss'
-import kuromoji from '../../modules/keitaiso.js'
+import kuromoji from "kuromoji"
+
+const KuromojiBuilder = kuromoji.builder({ dicPath: "/dict" });
 
 // typeScriptの場合は「interface」でState管理している
 interface setState {
-  word: string;
-  wordList: [],
+  input_text: string;
+  wordList: string[];
 }
 
-interface Keitaiso {
-  pos: string,
-  pos_detail_1: string,
-}
+class CreateWords extends React.Component {
+  constructor(props: any) {
+    super(props);
+    this.state = {
+      input_text: '',
+      wordList: [],
+    };
 
-class WordManage extends React.Component {
+    this.textChange = this.textChange.bind(this);
+    this.runKuromoji = this.runKuromoji.bind(this);
+  }
   public componentDidMount(){
-    var list = kuromoji(
-        `今日はクマを見て楽しかった。`
-      , (v: Keitaiso)=>{
-        return v.pos==='名詞' && v.pos_detail_1==="一般";
-      }
-    );
-    console.log(list);
   }
 
+  public textChange(event: any) {
+    this.setState({input_text: event.target.value});
+  }
+  public runKuromoji(event: any) {
+    const text = this.state.input_text;
+    if(text) KuromojiBuilder.build((err, tokenizer) => {
+      if(err){
+        console.log(err)
+      } else {
+        const tokens = tokenizer.tokenize(text).filter( v=>v.pos==='名詞' && v.pos_detail_1==="一般");
+        console.log(tokens);
+        let list:string[] = [];
+        tokens.forEach(w=>{
+          list.push(w.surface_form);
+        });
+        list = list.filter((a,b,c)=>{return c.indexOf(a)===b;});
+        this.setState({
+          wordList: list,
+        });
+      }
+    });
+  }
   // stateを入れる
   public state: setState = {
-    word: '質問',
+    input_text: '',
     wordList: [],
   }
 
   public render() {
-    console.log(this.state.wordList);
     return (
       <div className="driver">
         <h1>言葉作成画面</h1>
-        <span>
+        <div>
+          <textarea value={this.state.input_text} onChange={this.textChange} />
+          <button onClick={this.runKuromoji}>作成</button>
+        </div>
         <ul className="wordList">
+          {
+            this.state.wordList.map(x => (<li>{x}</li>))
+            }
         </ul>
-        </span>
       </div>
     );
   }
 }
-export default WordManage
+export default CreateWords
