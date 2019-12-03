36a1901c28d2ea1c939c557bb3e723de5f7ae7f2
yun <>
2019-12-03 16:24:37 +0900

    Firestore登録を追加

--- websites/src/page/menu/createWords.tsx
+++ websites/src/page/menu/createWords.tsx
@@ -1,13 +1,19 @@
 import * as React from 'react'
 import '../../scss/app.scss'
 import kuromoji from "kuromoji"
+import firebase from '../../firebase/firebase'
+import admin from 'firebase/app';
 
+
+const database = firebase.firestore();
+const animalRef = database.collection('wordset').doc('animal');
 const KuromojiBuilder = kuromoji.builder({ dicPath: "/dict" });
 
 // typeScriptの場合は「interface」でState管理している
 interface setState {
   input_text: string;
   wordList: string[];
+  nowList: string[];
 }
 
 class CreateWords extends React.Component {
@@ -16,12 +22,21 @@ class CreateWords extends React.Component {
     this.state = {
       input_text: '',
       wordList: [],
+      nowList: [],
     };
 
     this.textChange = this.textChange.bind(this);
     this.runKuromoji = this.runKuromoji.bind(this);
+    this.addWord = this.addWord.bind(this);
+
   }
-  public componentDidMount(){
+  public async componentDidMount(){
+    const data = await animalRef.get();
+    const dd:any = data.data();
+    console.log(dd["words"]);
+    this.setState({
+      nowList: dd["words"],
+    })
   }
 
   public textChange(event: any) {
@@ -33,7 +48,8 @@ class CreateWords extends React.Component {
       if(err){
         console.log(err)
       } else {
-        const tokens = tokenizer.tokenize(text).filter( v=>v.pos==='名詞' && v.pos_detail_1==="一般");
+        console.log(tokenizer.tokenize(text));
+        const tokens = tokenizer.tokenize(text).filter( v=>v.pos==='名詞' && (v.pos_detail_1==="固有名詞" || v.pos_detail_1==="一般"));
         console.log(tokens);
         let list:string[] = [];
         tokens.forEach(w=>{
@@ -46,10 +62,20 @@ class CreateWords extends React.Component {
       }
     });
   }
+
+  public addWord(event: any){
+    const word = event.currentTarget.getAttribute('data-value');
+    if(word){
+      animalRef.update({
+        words: admin.firestore.FieldValue.arrayUnion(word)
+      });
+    }
+  }
   // stateを入れる
   public state: setState = {
     input_text: '',
     wordList: [],
+    nowList: []
   }
 
   public render() {
@@ -58,11 +84,17 @@ class CreateWords extends React.Component {
         <h1>言葉作成画面</h1>
         <div>
           <textarea value={this.state.input_text} onChange={this.textChange} />
-          <button onClick={this.runKuromoji}>作成</button>
+          {
+            this.state.nowList.length>0 && (
+              <button onClick={this.runKuromoji}>作成</button>
+            )
+          }
         </div>
         <ul className="wordList">
           {
-            this.state.wordList.map(x => (<li>{x}</li>))
+            this.state.wordList.map(x => (
+              <li key={x}><span>{x}</span><button data-value={x} onClick={this.addWord}>{x}</button></li>
+            ))
             }
         </ul>
       </div>
